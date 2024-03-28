5b673712783553bc99c8e20911d9ca044a0fd3ad
Luiz Bataioli <luizbataioli23@gmail.com>
2024-03-27 21:53:31 -0300

    Root

--- root/js/index.js
+++ root/js/index.js
@@ -1,33 +1,33 @@
-function validaBusca(){
-    if(document.querySelector('#q').value==''){
+function validaBusca() {
+    if (document.querySelector('#q').value == '') {
         alert('Não podia ter deixado em branco a busca!')
         return false;
     }
 }
 
-document.querySelector('#form-busca').onsubmit=validaBusca;
+document.querySelector('#form-busca').onsubmit = validaBusca;
 
-var banner = ["./img/html.png","./img/css.png","./img/js.png"]
+var banner = ["./img/html.png", "./img/css.png", "./img/js.png"]
 var bannerAtual = 0;
-function trocaBanner(){
+function trocaBanner() {
     bannerAtual = (bannerAtual + 1) % 3;
-    document.querySelector('.destaque img').src = banner[bannerAtual];  
+    document.querySelector('.destaque img').src = banner[bannerAtual];
 }
 var timer = setInterval(trocaBanner, 1000);
 
 var controle = document.querySelector('.pause');
-controle.onclick = function() { 
-    if (controle.className == 'pause') { 
+controle.onclick = function () {
+    if (controle.className == 'pause') {
         clearInterval(timer);
         controle.className = 'play';
-    } 
-    else { 
+    }
+    else {
         timer = setInterval(trocaBanner, 1000);
-        controle.className = 'pause'; 
-        
-    } 
-    return false; 
-}; 
+        controle.className = 'pause';
+
+    }
+    return false;
+};
 
 
 
